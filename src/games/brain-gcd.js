4021c8112a2683eb42819179d0a6c3acebfed875
Анастасия Фрисман <ruzavinaanastasiya1@gmail.com>
2026-03-27 15:40:38 +0300

    Feat: add brain-gcd game
    - Add GCD game with Euclidean algorithms
    - Update package.json and Makefile

---
+++ src/games/brain-gcd.js
@@ -0,0 +1,30 @@
+import playGame from '../index.js'
+
+const getRandomNumber = () => Math.floor(Math.random() * 101)
+
+const generateRound = () => {
+  let a = getRandomNumber()
+  let b = getRandomNumber()
+  const question = `${a} ${b}`
+  let correctAnswer
+
+  if (b === 0) {
+    correctAnswer = String(a)
+  }
+  else {
+    let result
+    while (b !== 0) {
+      result = a % b
+      a = b
+      b = result
+    }
+  correctAnswer = String(a)
+  }
+
+
+return [question, correctAnswer]
+}
+
+const gameDescription = 'Find the greatest common divisor of given numbers.'
+
+export const playGcdGame = () => playGame(generateRound, gameDescription)
